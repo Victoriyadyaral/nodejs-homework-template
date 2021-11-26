6191b8e3560e2eedf39e55916a6b1d41ea96c7b7
Вікторія Щербанич <dyaral696@gmail.com>
2021-11-26 13:49:30 +0200

    added tests to authenticate middlewares

---
+++ tests/__tests__/authenticate.test.js
@@ -0,0 +1,72 @@
+const { Unauthorized } = require('http-errors')
+const authenticate = require('../../middlewares/authenticate')
+const { User } = require('../../models/user')
+
+const user = {
+  _id: '619d614b07cb3a9cd30b5eb8',
+  email: 'evd@example.com',
+  subscription: 'starter',
+  avatarURL: '\\users\\619d6e5dba5b038224aaffe3\\619d6e5dba5b038224aaffe3_24-11-2021_12-37-56_images.jpeg',
+  token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOWQ2ZTVkYmE1YjAzODIyNGFhZmZlMyIsImlhdCI6MTYzNzc0ODgwMiwiZXhwIjoxNjM3NzUyNDAyfQ.8pPavuPu7xRj3vUyF1o2V7bO_vVtT07vguHvWuSEe4U',
+  password: '$2a$10$cVs7Als2w3nmrwo2IH8Yr.dLRea5euHnLUV/6lBy99.ds3gaiBVWK',
+  createdAt: '2021-11-23T22:42:37.360Z',
+  updatedAt: '2021-11-24T10:37:56.607Z'
+}
+
+jest.spyOn(User, 'findById').mockImplementationOnce(() => user)
+
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxOWQ2ZTVkYmE1YjAzODIyNGFhZmZlMyIsImlhdCI6MTYzNzc0ODgwMiwiZXhwIjoxNjM3NzUyNDAyfQ.8pPavuPu7xRj3vUyF1o2V7bO_vVtT07vguHvWuSEe4U'
+
+const mockReq = {
+  headers: {
+    authorization: `Bearer ${token}`
+  },
+  user
+}
+
+const mockRes = {}
+
+const mockNext = jest.fn()
+
+describe('tests for middleware by authorization', function () {
+  test('without Bearer in the header', async () => {
+    const mockReq = {
+      headers: {
+        authorization: ` ${token}`
+      }
+    }
+
+    await authenticate(mockReq, mockRes, mockNext)
+    expect(mockNext).toHaveBeenCalledWith(new Unauthorized())
+  })
+
+  it('token is not valid in the header', async () => {
+    const token = 'eyJhbGci'
+    const mockReq = {
+      headers: {
+        authorization: `Bearer ${token}`
+      }
+    }
+
+    await authenticate(mockReq, mockRes, mockNext)
+    expect(mockNext).toHaveBeenCalledWith(new Unauthorized('jwt malformed'))
+  })
+
+  it('without token in the header', async () => {
+    const mockReq = {
+      headers: {
+        authorization: 'Bearer '
+      }
+    }
+
+    await authenticate(mockReq, mockRes, mockNext)
+    expect(mockNext).toHaveBeenCalledWith(new Unauthorized('jwt must be provided'))
+  })
+
+  it('token isvalid in the header', async () => {
+    await authenticate(mockReq, mockRes, mockNext)
+    expect(mockReq.headers.authorization).toBe(`Bearer ${token}`)
+    expect(mockReq.user._id).toEqual(user._id)
+    expect(mockNext).toHaveBeenCalled()
+  })
+})
